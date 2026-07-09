c57f4dd9b44b755e1c86ff094798028ef6415c44
Aladin <aladinzecic10@email.com>
2026-07-09 11:16:04 +0200

    aaaaaa

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,29 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/css/login.css',
+                'resources/css/mainview.css',
+                'resources/css/controllerview.css',
+                'resources/css/controllerdashboard.css',
+                'resources/css/dispatcherrequests.css',
+                'resources/css/dispatcherfleet.css',
+                'resources/css/livemapp.css',
+                'resources/css/drivers.css',
+                'resources/css/dashboard.css',
+                'resources/css/myshift.css',
+                'resources/css/predeparture.css',
+                'resources/css/faults.css',
+                'resources/css/workorders.css',
+                'resources/css/inspections.css',
+                'resources/css/tickets.css',
+                'resources/css/schedules.css',
+                'resources/js/app.js',
+            ],
             refresh: true,
         }),
-        tailwindcss(),
     ],
-    server: {
-        watch: {
-            ignored: ['**/storage/framework/views/**'],
-        },
-    },
 });
